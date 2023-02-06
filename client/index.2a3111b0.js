0477312362870e5d0ef0c25381e2414bc906dbc5
GitHub Pages <gh-pages@github.com>
2023-02-06 07:54:53 +0000

    deploy: 26455941830c64fad73608df9f15bf62f35659c8

---
+++ client/index.2a3111b0.js
@@ -0,0 +1,1 @@
+import{S as t,i as s,s as r}from"./client.dc10c6c5.js";function e(){return this.redirect(301,"/about")}export default class extends t{constructor(t){super(),s(this,t,null,null,r,{})}}export{e as preload};
