fc368e0bcca02fdf2af79af4a4194b4ed0fac7d8
GitHub Pages <gh-pages@github.com>
2023-04-26 00:50:07 +0000

    deploy: eb18a14cc517c4566b613ff17acbcb9e95bed763

---
+++ client/index.9aab1c79.js
@@ -0,0 +1,1 @@
+import{S as t,i as s,s as e}from"./client.c7012e23.js";function r(){return this.redirect(301,"/about")}export default class extends t{constructor(t){super(),s(this,t,null,null,e,{})}}export{r as preload};
--- client/index.d5848c1e.js
+++
@@ -1,1 +0,0 @@
-import{S as t,i as s,s as e}from"./client.5bb89e55.js";function r(){return this.redirect(301,"/about")}export default class extends t{constructor(t){super(),s(this,t,null,null,e,{})}}export{r as preload};
